306cff45961ef2d60271d9c9f5bc9e7c5fc614a6
Henry Smith <henry@henrysmith.org>
2018-06-27 21:52:28 +0200

    Hook up runtime with some syntax error highlighting

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,9 +1,11 @@
 const editor = require("./editor").default;
+const eightbit = require("./8bit").default;
 const global = require("./global").default;
 const viewport = require("./viewport").default;
 
 const actions = Object.assign({},
   editor,
+  eightbit,
   global,
   viewport,
 );
--- app/middlewares/editor.js
+++ app/middlewares/editor.js
@@ -1,19 +1,21 @@
-//require("codemirror/mode/lua/lua");
-//require("codemirror/addon/edit/closebrackets");
+require("codemirror/mode/lua/lua");
+require("codemirror/addon/edit/closebrackets");
 
 const CodeMirror = require("codemirror");
 
 const { createMiddleware } = require("signalbox");
 const actions = require("../actions").default;
 
+let cm;
+const markers = [];
+
 export const middleware = createMiddleware((cancel, before, after) => ({
   [after(actions.START)](store, action) {
-
     const textarea = document.querySelector('.Cart__textarea');
 
-    const cm = CodeMirror.fromTextArea(textarea, {
+    cm = CodeMirror.fromTextArea(textarea, {
       lineNumbers: true,
-      //mode: "lua",
+      mode: "lua",
       autofocus: true,
       indentWithTabs: false,
       indentSize: 2,
@@ -25,6 +27,33 @@ export const middleware = createMiddleware((cancel, before, after) => ({
     cm.on("keyup", () => {
       store.dispatch(actions.changeCode(cm.getValue()));
     })
+  },
+
+  [after(actions.CHANGE_CODE)](store, action) {
+    markers.forEach(marker => marker.clear());
+  },
+
+  [after(actions.SYNTAX_ERROR)](store, action) {
+    const line = action.line;
+    const column = action.column;
+
+    const from = {
+      line: line - 1,
+      ch: column - 2,
+    }
+
+    const to = {
+      line: line - 1,
+      ch: column - 1,
+    };
+
+    const options = {
+      className: 'syntax-error',
+    };
+
+    setTimeout(() => {
+      markers.push(cm.markText(from, to, options));
+    }, 100);
   }
 }));
 
