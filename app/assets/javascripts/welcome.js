680b50eebe187f77432dde8f50ec1b4fffbb5811
Henry Catalini Smith <henry@catalinismith.com>
2018-06-27 19:55:46 +0200

    Install codemirror

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,2 +1,15 @@
-console.log('8bitc.art');
+document.addEventListener('DOMContentLoaded', () => {
+  const textarea = document.querySelector('.Cart__textarea');
+
+  const cm = CodeMirror.fromTextArea(textarea, {
+    lineNumbers: true,
+    //mode: "lua",
+    autofocus: true,
+    indentWithTabs: false,
+    indentSize: 2,
+    indentUnit: 1,
+    smartIndent: true,
+    cursorBlinkRate: 500,
+  });
+});
 
