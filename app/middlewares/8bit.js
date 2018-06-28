002ebc9b9aed08c4f5ba76062315dd43bba17d18
Henry Smith <henry@henrysmith.org>
2018-06-28 19:05:13 +0200

    lots of shit dunno lol

--- app/middlewares/8bit.js
+++ app/middlewares/8bit.js
@@ -1,14 +1,55 @@
-const { parse } = require("../8bit");
-const eightbit = require("../8bit").default;
+const { parse, render } = require("../8bit");
 
 const { createMiddleware } = require("signalbox");
 const actions = require("../actions").default;
 const select = require("../reducers/selectors").default;
 
+let canvas;
+let workerPath;
+let worker;
+let raf;
+let running = true;
+let memory = new Uint8Array(0x8000);
+
+memory[0x7000] = 9; // one yellow pixel halfway down the screen
+
 export const middleware = createMiddleware((cancel, before, after) => ({
+  [after(actions.START)](store, action) {
+    workerPath = document.querySelector('#worker').textContent.trim();
+    worker = new Worker(workerPath);
+    canvas = document.querySelector('canvas');
+
+    worker.onmessage = ({ data }) => {
+      switch (data.type) {
+        case 'CODE_FINISHED':
+          running = false;
+          break;
+
+        case 'POKE':
+          //console.log(`${data.address}: ${data.value}`);
+          memory[data.address] = data.value;
+          break;
+      }
+    }
+
+    worker.postMessage({
+      type: 'INITIALIZE',
+      memory,
+    });
+
+    raf = () => {
+      render(memory, canvas);
+      if (running) {
+        requestAnimationFrame(raf);
+      } else {
+        console.log('stopping');
+      }
+    };
+    requestAnimationFrame(raf);
+  },
+
   [after(actions.CHANGE_CODE)](store, action) {
     const state = store.getState();
-    const canvas = document.querySelector('canvas');
     const code = select("editor").from(state).code();
 
     let ast;
@@ -23,7 +64,11 @@ export const middleware = createMiddleware((cancel, before, after) => ({
       return;
     }
 
-    eightbit(code, canvas);
+    running = true
+    worker.postMessage({
+      type: 'RUN_CODE',
+      code,
+    });
   }
 }));
 
