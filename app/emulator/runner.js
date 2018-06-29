b6ba263be5ce97ee19746550dde3f146ed8221ff
Henry Smith <henry@henrysmith.org>
2018-06-29 14:59:58 +0200

    first unit test yay

--- app/emulator/runner.js
+++ app/emulator/runner.js
@@ -1,12 +1,12 @@
 const lua2js = require("lua2js");
 const saferEval = require("safer-eval");
 
+const api = require("./api").default;
+
 export default function run(code, memory) {
   const evalContext = {
     __lua: lua2js.stdlib.__lua,
-    poke(address, value) {
-      memory[address] = value;
-    }
+    ...api(memory),
   };
 
   saferEval(code, evalContext)
