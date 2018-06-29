7dc4042cf3f3319e5ef8267c452b00a8792d12d4
Henry Catalini Smith <henry@catalinismith.com>
2018-06-29 18:48:42 +0200

    Add a bunch of stuff

--- app/emulator/api.js
+++ app/emulator/api.js
@@ -4,9 +4,28 @@ module.exports = memory => ({
   //atan2(dx, dy) {
   //},
 
+  band(first, second) {
+    return first & second;
+  },
+
+  bnot(num) {
+    return ~num;
+  },
+
+  bor(first, second) {
+    return first | second;
+  },
+
+  bxor(first, second) {
+    return first ^ second;
+  },
+
   cos(angle) {
     return Math.cos((angle * 360) * (π / 180))
   },
+
+  //lshr(num, bits) {
+  //},
 
   memset(destaddr, val, len) {
     memory.fill(val, destaddr, destaddr + len);
@@ -24,7 +43,27 @@ module.exports = memory => ({
     return Math.random() * max;
   },
 
+  shl(num, bits) {
+    return num << bits;
+  },
+
+  shr(num, bits) {
+    return num >> bits;
+  },
+
   sin(angle) {
     return Math.sin((-angle * 360) * (π / 180))
+  },
+
+  sub(str, start, end) {
+    if (end === undefined) {
+      if (start < 0) {
+        return str.substring(str.length + start, str.length);
+      } else {
+        return str.substring(start - 1);
+      }
+    } else {
+      return str.substring(start - 1, end - start + 1);
+    }
   },
 });
