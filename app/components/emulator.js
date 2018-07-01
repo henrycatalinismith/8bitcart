73009edb4eb11fd9595062b9724860b7476ddf74
Henry Smith <henry@henrysmith.org>
2018-07-01 16:29:31 +0200

    Finally sorting this mess out

--- app/components/emulator.js
+++ app/components/emulator.js
@@ -4,20 +4,12 @@ const PropTypes = require("prop-types");
 export default class Emulator extends React.Component {
   static propTypes = {
     children: PropTypes.object,
-    width: PropTypes.number,
-    height: PropTypes.number,
   };
 
   render() {
-    const { children, width, height } = this.props;
-    const style = {
-      width: `${width}px`,
-      height: `${height}px`,
-    };
-
     return (
-      <div className="emulator" style={style}>
-        {children}
+      <div className="emulator">
+        {this.props.children}
       </div>
     );
   }
