bd7dbc736de7bb0d9591506215c09b4eb8f888cd
Henry Smith <henry@henrysmith.org>
2018-07-05 22:51:55 +0200

    breakit

--- app/emulator/api.js
+++ app/emulator/api.js
@@ -8,14 +8,12 @@ const pxa = (x, y) => {
 }
 
 const pxc = (x, y, memory) => {
-  const addr = pxa(x, y);
-  const and = x & 1;
-  const add = and ? 1 : 0;
-  const px = memory[addr];
-  if (and) {
-    return px & 0xF;
+  const addr1 = 0x6000 + (64 * y) + Math.floor(x / 2);
+  const side = x % 2 === 0 ? 'left' : 'right';
+  if (side === 'left') {
+    return memory[addr1] >> 4;
   } else {
-    return px;
+    return memory[addr1] & 0x0f;
   }
 }
 
@@ -89,8 +87,18 @@ module.exports = memory => ({
   },
 
   pset(x, y, c) {
-    const addr = pxa(x, y);
-    memory[addr] = c;
+    const addr = 0x6000 + (y*64) + Math.floor(x/2);
+    const side = x % 2 === 0 ? 'left' : 'right';
+    const left = memory[addr] >> 4;
+    const right = memory[addr] & 0x0f;
+    if (side === 'left') {
+      memory[addr] = (c << 4) | right;
+    } else {
+      memory[addr] = (left << 4) | c;
+    }
+    //const addr = pxa(x, y);
+    //const even = x % 2 === 0;
+    //memory[addr] = c;
   },
 
   rnd(max) {
