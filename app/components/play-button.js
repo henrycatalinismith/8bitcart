34730015b706ad58bd4f2ed3afc28eb640634ece
Henry Smith <henry@henrysmith.org>
2018-07-05 21:00:38 +0200

    button hover

--- app/components/play-button.js
+++ app/components/play-button.js
@@ -8,18 +8,37 @@ export default class PlayButton extends React.Component {
     onClick: PropTypes.func,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = { focused: false };
+  }
+
+  onFocus = () => this.setState({ focused: true });
+  onBlur = () => this.setState({ focused: false });
+
   render() {
     const { playing, onClick } = this.props;
+    const { focused } = this.state;
+    const { onFocus, onBlur } = this;
+
     const className = classnames({
       "play-button": true,
       "play-button--playing": playing,
       "play-button--stopped": !playing,
+      "play-button--focused": focused,
     });
-    //const icon = playing ? "⏹" : "▶️";
+
     const text = playing ? "⏹ Stop" : "▶️ Run";
 
+    const props = {
+      className,
+      onClick,
+      onFocus,
+      onBlur
+    };
+
     return (
-      <button className={className} onClick={onClick}>
+      <button {...props}>
         {text}
       </button>
     );
